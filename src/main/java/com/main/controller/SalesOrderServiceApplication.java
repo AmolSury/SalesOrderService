735fa2f20af8b2f5e02fca19ad9ddc92006345e5
amolsury <asuryavanshi12@gmail.com>
2018-11-16 11:18:35 +0530

    update

--- src/main/java/com/main/controller/SalesOrderServiceApplication.java
+++ src/main/java/com/main/controller/SalesOrderServiceApplication.java
@@ -7,16 +7,20 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.client.RestTemplate;
+
+import com.main.util.RibbonConfiguration;
 
 
 @EnableCircuitBreaker
 @EnableHystrixDashboard
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "server", configuration = RibbonConfiguration.class)
 @ComponentScan({"com.main.controller", "com.main.services"})
 @EnableJpaRepositories("com.main.repository")
 @EntityScan("com.main.entity")
